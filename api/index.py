6d098bc57a5265e99b17cf37493d495c8b9568d4
Joel Kim <joelhtkim@gmail.com>
2025-04-03 22:04:19 -0500

    one line

--- api/index.py
+++ api/index.py
@@ -2,7 +2,8 @@ from flask import Flask, request, render_template
 
 from functions import impliedProbability, probabilityToAmerican, novig
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
